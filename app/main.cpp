258b8d827dce2a9f06bddb99ac823e4e101a595f
sereDIALLO <127400655+sereDIALLO@users.noreply.github.com>
2024-10-21 23:58:41 +0200

    Add files via upload

---
+++ app/main.cpp
@@ -0,0 +1,11 @@
+ #include "LaserDetection.hpp"
+
+int main() {
+    LaserDetection laserDetection("../data/im001.png");
+    laserDetection.processImage();
+    laserDetection.findIntersections();
+    laserDetection.displayResults();
+    laserDetection.saveResult("../resultats/test.png");
+
+    return 0;
+}
