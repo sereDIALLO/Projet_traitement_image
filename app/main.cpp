5bdb5aa7bcd9cc22d67189e80dedd91e9d09969a
Marwane BELHAMRA <146735818+BLHmarwane@users.noreply.github.com>
2024-10-23 01:39:46 +0200

    repertoir
    probleme de repertoir regler

--- app/main.cpp
+++ app/main.cpp
@@ -1,39 +1,44 @@
 #include "../include/ImageProcessor.hpp"
 #include <filesystem> // Pour parcourir le répertoire des images
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
-
-int main(int argc, char** argv) {
+using namespace cv;
+int main(int argc, char** argv) 
+{
     // Initialisation du répertoire d'entrée et de sortie
-    std::string inputDir = "../data/input";
-    std::string outputDir = "../data/output";
+    std::string inputDir = "/Users/mar1/HOME/Github/data1/input";
+    std::string outputDir = "/Users/mar1/HOME/Github/data1/output";
 
 
+        if (!std::__fs::filesystem::exists(inputDir)) {
+        std::cerr << "Le répertoire d'entrée n'existe pas : " << inputDir << std::endl;
+        return -1;
+    } else {
+        std::cout << "Répertoire d'entrée trouvé. Contenu : " << std::endl;
+        for (const auto& entry : std::__fs::filesystem::directory_iterator(inputDir)) {
+            std::cout << entry.path() << std::endl;
+        }
+    }
 
-
-    // Parcourir toutes les images du répertoire d'entrée
     for (const auto& entry : std::__fs::filesystem::directory_iterator(inputDir)) {
+        std::string inputImagePath = entry.path().string();
+        std::string fileName = entry.path().filename().string();
+        std::string outputImagePath = outputDir + "/" + fileName.substr(0, fileName.find_last_of('.')) + "_result.png";
 
-        std::string imagePath = entry.path().string();
+        // Charger l'image
+        Mat img = imread(inputImagePath);
+        if (img.empty()) {
+            std::cerr << "Erreur: Impossible de charger l'image : " << inputImagePath << std::endl;
+            continue;
+        }
 
         // Créer une instance de ImageProcessor pour traiter l'image
-        ImageProcessor processor(imagePath);
+        ImageProcessor processor(inputImagePath);
 
-        // Traiter l'image et sauvegarder le résultat dans data/output
-        std::string outputFileName = outputDir + "/" + entry.path().filename().string();
-        processor.processImage(outputFileName);
+        // Traiter l'image et sauvegarder le résultat
+        processor.processImage(outputImagePath);
     }
 
     return 0;
 }
-
-/*
-    ImageProcessor ImageProcessor("../data/input/im003.png");
-    ImageProcessor.processImage();
-    ImageProcessor.findIntersections();
-    ImageProcessor.displayResults();
-    ImageProcessor.saveResult("../resultats/test.png");
-
-    return 0;
-}
-*/
